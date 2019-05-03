44c04f684ecc948b0259e56f867a7d62e889e64b
murffious <titan.murff@gmail.com>
2019-05-02 19:14:31 -0600

    3hr

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,27 @@ import TextBody from '../components/TextBody';
 import fetch from 'isomorphic-unfetch';
 import MainImage from '../components/MainImage';
 import Video from '../components/Video';
+const { parse } = require('url');
 
-  
-  App.getInitialProps = async ({ req }) => {
-    const res = await fetch('https://interview-project-17987.herokuapp.com/api/article');
+// 3hr mark notes 
+// was going smooth so went for routing to then focus last 3 hours on styles - 
+// was not super fast like I thought since just learning next.js and usually do spa
+
+//Next hour//
+// layout and responsiveness 
+
+
+  // routing --> will upgrade if time and add nav bar etc 
+  App.getInitialProps = async ({ req, query }) => {
+    console.log(query)
+    // const parsedUrl = parse(req.url, true);
+    // const { pathname, query } = parsedUrl;
+    // console.log(parsedUrl, pathname, parsedUrl.path)
+    const res = await fetch(`https://interview-project-17987.herokuapp.com/api/article`);
     const json = await res.json();
     return { articles: json };
   };
+
   
   function App({ articles }) {
     console.log(articles)
