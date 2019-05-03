5dc26f30cec555d803cab4c6d8b41f2cea4b05d4
murffious <titan.murff@gmail.com>
2019-05-02 20:50:22 -0600

    fixed title and description on click change

--- components/MainImage.js
+++ components/MainImage.js
@@ -2,37 +2,42 @@ import React, { useState } from 'react';
 
 function MainImage(props) {
     const { images } = props;
-    const [img, setImg] = useState(images[0].mainUrl);
+    const [ img, setImg ] = useState(images[0].mainUrl);
+    const [ description, setDescription ] = useState(images[0].description);
+    const [  title, setTitle ] = useState(images[0].title);
 
     return (
         <div class="img-contianer">
-            <img width="100%" src={`https://${img}`} alt="main image" />
-            <h1>{images[0].title}</h1>
-            <h4>{images[0].description}</h4>
+            <img width="100%" src={`https://${img}`} alt={description} />
+            <h1>{title}</h1>
+            <h4>{description}</h4>
             <div className="thumbnails">
                 {images.map(image => (
                     <img
                     // put a good key in here
-                    className={img === image.mainUrl ?`img-border`:null}
+                    className={img === image.mainUrl ?`img-border`: null}
                     key={image.description}
                     src={`https://${image.thumbnailUrl}`}
                     alt={image.description}
-                    onClick={() => setImg(image.mainUrl)}
+                    onClick={() => {
+                        setImg(image.mainUrl);
+                        setDescription(image.description);
+                        setTitle(image.title);
+                    }}
                     />
                 ))}
                 </div>
             <style jsx>{`
-                p {
-                    font-family: 'Open Sans', sans-serif;
-                }
-                div {
-                
-                }
                 .img-container {
                     display: flex;
+                    font-family: 'Open Sans', sans-serif;
                 }
                 .img-border {
                     border: 4px solid black; 
+                }
+                .thumbnails {
+                    max-width: 75px;
+                    display: flex;
                 }
                 @media (max-width: 600px) {
                     div {
