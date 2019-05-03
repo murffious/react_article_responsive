16e3a748f44105617a4972652da577ad0d019b16
murffious <titan.murff@gmail.com>
2019-05-02 20:13:30 -0600

    border toggle working

--- components/MainImage.js
+++ components/MainImage.js
@@ -1,22 +1,23 @@
-import React from 'react';
-MainImage.updateMainImage = thumbnail => () => {
-    
-}
+import React, { useState } from 'react';
+
 function MainImage(props) {
     const { images } = props;
+    const [img, setImg] = useState(images[0].mainUrl);
+
     return (
         <div class="img-contianer">
-            <img width="100%" src={`https://${images[0].mainUrl}`} alt="main image" />
+            <img width="100%" src={`https://${img}`} alt="main image" />
             <h1>{images[0].title}</h1>
             <h4>{images[0].description}</h4>
             <div className="thumbnails">
-                {images.map(thumb => (
+                {images.map(image => (
                     <img
                     // put a good key in here
-                    key={thumb}
-                    src={`https://${thumb.thumbnailUrl}`}
-                    alt={thumb}
-                    onClick={MainImage.updateMainImage(thumb)}
+                    className={img === image.mainUrl ?`img-border`:null}
+                    key={image.description}
+                    src={`https://${image.thumbnailUrl}`}
+                    alt={image.description}
+                    onClick={() => setImg(image.mainUrl)}
                     />
                 ))}
                 </div>
@@ -29,6 +30,9 @@ function MainImage(props) {
                 }
                 .img-container {
                     display: flex;
+                }
+                .img-border {
+                    border: 4px solid black; 
                 }
                 @media (max-width: 600px) {
                     div {
